120ec427c6f35db25faa13886765cb5dd3e527c0
Paul Taylor <pltorigami@gmail.com>
2020-07-23 22:40:49 -0400

    added delete time functionality and made the times list labels create and destroy dynamically if < 10 times in the database

--- tkTimer.py
+++ tkTimer.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import random
 from datetime import datetime
 from functools import partial
+from tkinter import messagebox
 from sqlalchemy import create_engine, Column, Float, Integer, DateTime, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -24,7 +25,7 @@ class TimeModel(Base):
 class Edit:
     def __init__(self, master, labText):
         self.master = master
-        self.master.geometry('550x260')
+        self.master.geometry('580x320')
         self.master.title('Add Penalty')
 
         self.timeId = int(labText[:labText.index(':')])
@@ -33,7 +34,7 @@ class Edit:
         self.idLab = tk.Label(self.master, text=f'Solve {self.timeObj.id}:', font=('TkDefaultFont', 18))
         self.idLab.pack(pady=(10, 0))
         self.timeLab = tk.Label(self.master, text=Timer.niceTime(self.timeObj.time, Timer.precision, penalty=self.timeObj.penalty, dnfTime=True), font=('TkDefaultFont', 30), pady=20)
-        self.timeLab.pack()  # make this show +2 or DNF depending on self.timeObj.penalty (add some other args to Timer.niceTime())
+        self.timeLab.pack()
         self.scramLab = tk.Label(self.master, text=f'Scramble: {self.timeObj.scramble}', font=('TkDefaultFont', 18))
         self.scramLab.pack()
         niceDate = self.timeObj.date.strftime('%m-%d-%Y %H:%M:%S')
@@ -55,9 +56,36 @@ class Edit:
                 penLab.bind('<Leave>', Timer.changeBackground)
 
             penLab.bind('<Button-1>', self.editTime)
-            penLab.grid(row=0, column=index + 1, sticky=tk.N + tk.S)  # add a button to delete the solve
+            penLab.grid(row=0, column=index + 1, sticky=tk.N + tk.S)
 
-        self.penaltyFrame.pack(pady=(10, 0))
+        self.penaltyFrame.pack(pady=(10, 20))
+
+        self.bottomFrame = tk.Frame(self.master)
+        self.deleteButton = tk.Button(self.bottomFrame, text='Delete solve', font=('TkDefaultFont', 18), padx=5, pady=5, command=self.deleteSolve)
+        self.deleteButton.grid(row=0, column=0, padx=(0, 10))
+        self.exitButton = tk.Button(self.bottomFrame, text='Done', font=('TkDefaultFont', 18), padx=5, pady=5, command=self.master.destroy)
+        self.exitButton.grid(row=0, column=1)
+        self.bottomFrame.pack()
+
+    def deleteSolve(self):
+        confirmDelete = messagebox.askokcancel('Confirm deletion', 'Are you sure you want to delete this solve?')
+        if confirmDelete:
+            session.delete(self.timeObj)
+            session.commit()
+            aboveTimes = session.query(TimeModel).filter(TimeModel.id > self.timeObj.id).all()
+            for time in aboveTimes:
+                time.id -= 1
+            session.commit()
+            self.master.destroy()
+
+            if not aboveTimes:
+                Timer.timeLabel.configure(text='0.00')
+
+            if len(session.query(TimeModel).all()) < 10:  # shrink times list if < 10 times in the database
+                Timer.timesLabs[-1].destroy()
+                del Timer.timesLabs[-1]
+
+            Timer.refreshTimes()
 
     def editTime(self, event):
         for widget in self.penaltyFrame.grid_slaves():
@@ -108,7 +136,7 @@ class Timer:
 
         rawLast = session.query(TimeModel).order_by(TimeModel.id.desc())
 
-        for index, timeObj in enumerate(rawLast.limit(10).all()):  # this list does not work when there are < 10 times in the database
+        for index, timeObj in enumerate(rawLast.limit(10).all()):
             timeLab = tk.Label(self.timesFrame, text=f'{timeObj.id}: {Timer.niceTime(timeObj.time, Timer.precision, penalty=timeObj.penalty)}')
             timeLab.bind('<Button-1>', self.editTime)
             timeLab.bind('<Enter>', Timer.changeBackground)
@@ -125,7 +153,8 @@ class Timer:
     @staticmethod
     def refreshTimes():
         rawLast = session.query(TimeModel).order_by(TimeModel.id.desc()).limit(10).all()
-        Timer.timeLabel.configure(text=Timer.niceTime(rawLast[0].time, precision=Timer.precision, penalty=rawLast[0].penalty))
+        if Timer.timeLabel['text'] != '0.00':
+            Timer.timeLabel.configure(text=Timer.niceTime(rawLast[0].time, precision=Timer.precision, penalty=rawLast[0].penalty))
         for timeObj, timeLab in zip(rawLast, Timer.timesLabs):
             timeLab.configure(text=f'{timeObj.id}: {Timer.niceTime(timeObj.time, precision=Timer.precision, penalty=timeObj.penalty)}')
 
@@ -137,8 +166,10 @@ class Timer:
             else:
                 event.widget.configure(background='White')
 
-    def openOptions(self):
+    def openOptions(self):  # let this control how many times to show in the list?
         pass  # but like probably open up different window (so different class)
+        # make these choices register in the db so that they are saved from session to session
+        # number precision, amount of times to show, color theme, timer font
 
     def makeScramble(self):  # generate random move 3x3 scramble
         scramble = ''
@@ -146,7 +177,7 @@ class Timer:
         prevchoice = 'Z'
         oldchoice = 'X'
         while True:
-            choice = random.choice(Timer.moveslist)  # MAKE THIS A STATIC METHOD DUMMY BOYYYYYYYYYYYYYYYYYYYY (maybe?)
+            choice = random.choice(Timer.moveslist)
             if i == 20:
                 break
             elif choice[0] == prevchoice[0]:
@@ -161,9 +192,17 @@ class Timer:
         Timer.scram = scramble.rstrip()
 
     def addTime(self):
-        timeObj = TimeModel(time=Timer.solveFloat, scramble=Timer.scram, date=Timer.solveStart)  # add time to database
-        session.add(timeObj)
+        timeObj = TimeModel(time=Timer.solveFloat, scramble=Timer.scram, date=Timer.solveStart)
+        session.add(timeObj)  # add time to database
         session.commit()
+
+        if len(Timer.timesLabs) < 10:  # dynamically add labels if < 10 times in the database
+            timeLab = tk.Label(self.timesFrame)
+            timeLab.bind('<Button-1>', self.editTime)
+            timeLab.bind('<Enter>', Timer.changeBackground)
+            timeLab.bind('<Leave>', Timer.changeBackground)
+            timeLab.grid(row=len(Timer.timesLabs) + 1, column=0, sticky=tk.W + tk.E)
+            Timer.timesLabs.append(timeLab)
 
         oldText = f'{timeObj.id}: {Timer.niceTime(Timer.solveFloat, Timer.precision)}'
 
